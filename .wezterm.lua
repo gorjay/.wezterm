8d57841756b0ec11cba1a78437c8ae81d571e6cf
gorjay <gorjay.huang@gmail.com>
2024-08-03 08:32:01 +0800

    update config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,79 +1,62 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
+config = wezterm.config_builder()
 
--- Basic
--- config.color_scheme = "Monokai Pro (Gogh)"
--- config.color_scheme = "Batman"
-config.color_scheme = "Catppuccin Mocha"
+-- config.color_scheme = "Catppuccin Mocha"
+-- config.color_scheme = "Catppuccin Macchiato"
+config.color_scheme = "Seafoam Pastel (Gogh)"
+
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	config.default_prog = { "C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe" }
+    config.default_prog = { "C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe" }
 end
 config.window_background_opacity = 1.0
 config.warn_about_missing_glyphs = false
 config.check_for_updates = false
 
--- Function
-wezterm.on("toggle-opacity", function(window, pane)
-	local overrides = window:get_config_overrides() or {}
-	if not overrides.window_background_opacity then
-		overrides.window_background_opacity = 0.8
-	else
-		overrides.window_background_opacity = nil
-	end
-	window:set_config_overrides(overrides)
-end)
-
---- Key bindings
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
-	-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
-	{
-		key = "a",
-		mods = "LEADER|CTRL",
-		action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
-	},
-	-- Toggle opacity
-	{
-		key = "b",
-		mods = "LEADER|CTRL",
-		action = wezterm.action.EmitEvent("toggle-opacity"),
-	},
+    { key = "a", mods = "LEADER|CTRL", action = act.SendKey({ key = "a", mods = "CTRL" }),},
+    { key = "n", mods = "LEADER|CTRL", action = act.SendString 'printf "\\e]9;%s\\e\\\\" "finished"'},
+    { key = "b", mods = "LEADER|CTRL", action = act.EmitEvent("toggle-opacity"),},
+    { key = 'Tab', mods = 'CTRL', action = act.ActivateLastTab,},
+    { key = "-", mods = "LEADER|CTRL", action = act.SplitVertical { domain = "CurrentPaneDomain" },},
+    { key = "\\",mods = "LEADER|CTRL", action = act.SplitHorizontal { domain = "CurrentPaneDomain" },},
+    { key = "h", mods = "LEADER|CTRL", action = act.ActivatePaneDirection("Left"),},
+    { key = "j", mods = "LEADER|CTRL", action = act.ActivatePaneDirection("Down"),},
+    { key = "k", mods = "LEADER|CTRL", action = act.ActivatePaneDirection("Up"),},
+    { key = "l", mods = "LEADER|CTRL", action = act.ActivatePaneDirection("Right"),},
+    { key = 'w', mods = 'LEADER|CTRL', action = act.PaneSelect{ alphabet = '1234567890', }},
+    { key = ',', mods = 'LEADER|CTRL', action = act.PaneSelect{ alphabet = '1234567890', mode = 'SwapWithActive' }},
+    { key = 'H', mods = 'LEADER', action = act.AdjustPaneSize { 'Left', 5 }, },
+    { key = 'J', mods = 'LEADER', action = act.AdjustPaneSize { 'Down', 5 }, },
+    { key = 'K', mods = 'LEADER', action = act.AdjustPaneSize { 'Up', 5 } },
+    { key = 'L', mods = 'LEADER', action = act.AdjustPaneSize { 'Right', 5 }, },
+    { key = 'z', mods = 'CTRL', action = act.TogglePaneZoomState,},
+    { key = 'q', mods = 'LEADER|CTRL', action = wezterm.action.CloseCurrentPane { confirm = true },},
 }
--- for i = 1, 8 do
---   -- ALT + number to activate that tab
---   table.insert(config.keys, {
---     key = tostring(i),
---     mods = 'ALT',
---     action = wezterm.action.ActivateTab(i - 1),
---   })
---   -- F1 through F8 to activate that tab
---   table.insert(config.keys, {
---     key = 'F' .. tostring(i),
---     action = wezterm.action.ActivateTab(i - 1),
---   })
--- end
-
 config.mouse_bindings = {
-	-- Scrolling up while holding CTRL increases the font size
-	{
-		event = { Down = { streak = 1, button = { WheelUp = 1 } } },
-		mods = "CTRL",
-		action = wezterm.action.IncreaseFontSize,
-	},
-
-	-- Scrolling down while holding CTRL decreases the font size
-	{
-		event = { Down = { streak = 1, button = { WheelDown = 1 } } },
-		mods = "CTRL",
-		action = wezterm.action.DecreaseFontSize,
-	},
+    { event = { Down = { streak = 1, button = { WheelUp = 1 } } }, mods = "CTRL", action = act.IncreaseFontSize,    },
+    { event = { Down = { streak = 1, button = { WheelDown = 1 } } }, mods = "CTRL",    action = act.DecreaseFontSize, },
 }
 
--- and finally, return the configuration to wezterm
+-- F1 through F8 to activate that tab
+for i = 1, 8 do
+  table.insert(config.keys, {
+    key = 'F' .. tostring(i),
+    action = wezterm.action.ActivateTab(i - 1),
+  })
+end
+
+wezterm.on("toggle-opacity", function(window, pane)
+    local overrides = window:get_config_overrides() or {}
+    if not overrides.window_background_opacity then
+        overrides.window_background_opacity = 0.8
+    else
+        overrides.window_background_opacity = nil
+    end
+    window:set_config_overrides(overrides)
+end)
+
 return config
